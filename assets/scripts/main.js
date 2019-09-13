5a1f51227e04e0251618d56f75d867333ca07323
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-09-13 13:03:45 -0500

    finished basic html and added styling

---
+++ assets/scripts/main.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// !!! HTML
+/*
+    1. fix structure
+    2. menu
+
+*/
+
+// !!! CSS
+/*
+    1. overflow: scroll
+    2. styles to customize scroll bar
+    3. 360 / max time
+*/
+
+// !!! JS
+/*
+    0. when play button is pressed then do stuff
+    Default style for counter = work option
+    1. Generate option page
+    2. round donut needs to change colors corresponding with current option, red = work, short break = gree, long break = blue
+    3. round numbers 1/4 needs to increment by 1 everytime we hit 00:00 (work)
+    4. sound files
+    5. volume control:https://www.w3schools.com/tags/av_prop_volume.asp    
+
+*/
---
+++ clock_app/js.js
@@ -0,0 +1,32 @@
+let counter = 0;
+let secondscounter = 0;
+let minutescounter = 0;
+let hourscounter = 0;
+function setDate() {
+    const NOW = new Date();
+    const SECONDS = NOW.getSeconds();
+    const MINUTES = NOW.getMinutes();
+    const HOURS = NOW.getHours();
+    const SECONDSDEGREES = ((((SECONDS / 60) *360) + 90)+secondscounter);
+    const MINUTESDEGREES = ((((MINUTES / 60) * 360) + 90)+minutescounter);
+    const HOURSDEGREES = (((((HOURS / 12) * 360) + 90))+hourscounter);
+    SECONDHAND.style.transform = `rotate(${SECONDSDEGREES}deg)`;
+    MINUTEHAND.style.transform = `rotate(${MINUTESDEGREES}deg)`;
+    HOURHAND.style.transform = `rotate(${HOURSDEGREES}deg)`;
+    if (SECONDS == 59) {
+        secondscounter += 360;
+    }
+    if (MINUTES == 59) {
+        minutescounter += 360;
+    }
+    if (HOURS == 11) {
+        hourscounter += 360;
+    }
+}
+
+const SECONDHAND = document.querySelector('.second-hand');
+const MINUTEHAND = document.querySelector('.min-hand');
+const HOURHAND = document.querySelector('.hour-hand');
+const HAND = document.querySelector('.hand');
+
+setInterval(setDate, 1000);
